dacef3601adea4da55135c4983cdb3ae45fcabd7
thanapornPit <thanaporn8793@gmail.com>
2025-10-16 13:13:02 +0700

    Delete app directory

--- app/utils/security.py
+++
@@ -1,4 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-def hash_password(p): return generate_password_hash(p)
-def verify_password(p, h): return check_password_hash(h, p)
